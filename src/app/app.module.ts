fc6e936d0578500ce70fce7e6566f4b1bfc48dc9
Prem DMA <prem.wanderer@gmail.com>
2018-11-17 22:31:03 +0530

    Adding new files

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { MaterialModule} from './material.module';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { routing } from './app.routing';
+import 'hammerjs';
+
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { LoginComponent } from './components/login/login.component';
+import { LoginService } from './services/login.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    HttpModule,
+    routing,
+    FormsModule,
+  ],
+
+  bootstrap: [AppComponent],
+
+  providers: [LoginService]
+})
+export class AppModule { }
---
+++ src/app/material.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+
+import {
+  MatButtonModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule,
+  MatInputModule,
+  MatGridListModule
+} from '@angular/material';
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatGridListModule,
+    MatInputModule,
+  ],
+  exports: [
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatGridListModule,
+    MatInputModule,
+  ]
+})
+export class MaterialModule {}
